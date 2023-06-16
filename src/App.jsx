c39cdeffc9716c62473b8bd7669ebf6384504941
ZinoTrust <zinotrust@gmail.com>
2023-06-16 14:24:19 +0100

    reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import TaskManager from "./components/taskManager/TaskManager";
 import TaskManagerReducer from "./components/taskManager/TaskManagerReducer";
 
+// To use the Task Manager with use Reducer
+// Uncomment it below
+
 function App() {
   return (
     <div>
       <TaskManager />
+      {/* <TaskManagerReducer /> */}
     </div>
   );
 }
